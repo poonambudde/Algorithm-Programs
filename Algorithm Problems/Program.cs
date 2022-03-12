db9ed0ba2f3b3c97d04b9ae5e55d523665e50a1d
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-12 15:23:17 +0530

    [Poonam] ADDED: Check wether Two Strings are Anagram or not

--- Algorithm Problems/Program.cs
+++ Algorithm Problems/Program.cs
@@ -7,23 +7,8 @@ namespace Algorithm_Problems
     {
         public static void Main()
         {
-            string str1 = "heart";
-            string str2 = "earth";
-            char[] ch1 = str1.ToLower().ToCharArray();
-            char[] ch2 = str2.ToLower().ToCharArray();
-            Array.Sort(ch1);
-            Array.Sort(ch2);
-            string val1 = new string(ch1);
-            string val2 = new string(ch2);
-
-            if (val1 == val2)
-            {
-                Console.WriteLine("Both the strings are Anagrams");
-            }
-            else
-            {
-                Console.WriteLine("Both the strings are not Anagrams");
-            }
+            Anagram anagram = new Anagram();
+            anagram.Check_Anagram();
         }
     }
 }

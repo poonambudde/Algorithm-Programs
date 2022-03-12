6fd083f207887097f6f87fa6ea4e47d832f2d3eb
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-12 15:27:11 +0530

    [Poonam] ADDED: static functions to return all permutations of a String

--- Algorithm Problems/Program.cs
+++ Algorithm Problems/Program.cs
@@ -1,40 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Algorithm_Problems
 {
-    class Permutations
+    class Program
     {
-        // Recursive function to generate all permutations of a string
-        public static void Permutate(string str, int start, int end)
+        static void Main(string[] args)
         {
-            if (start == end)
-            {
-                Console.WriteLine(str);
-            }
-            else
-            {
-                for (int i = start; i <= end; i++)
-                {
-                    str = (Swap(str, start, i));
-                    Permutate(str, start + 1, end);
-                    str = Swap(str, start, i);
-                }
-            }
+            Console.WriteLine("Welcome to Data Sructures Algorithm Programs");
 
-        }
-
-        //swap two characters in a character array
-        public static string Swap(string word, int start, int end)
-        {
-            char temp;
-            char[] charArray = word.ToCharArray();
-            temp = charArray[start];
-            charArray[start] = charArray[end];
-            charArray[end] = temp;
-            string s = new string(charArray);
-            return s;
+            Console.WriteLine("Enter String to find Permutation ");
+            string str = Console.ReadLine();
+            int n = str.Length;
+            Permutations.Permutate(str, 0, n - 1);
         }
     }
 }

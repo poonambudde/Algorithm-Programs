2085e8603d68bd07f5d79eefacee8931654fbb19
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-12 15:59:15 +0530

    [Poonam] ADDED: Find the Prime numbers in the range of 0 - 1000

--- Algorithm Problems/Program.cs
+++ Algorithm Problems/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Algorithm_Problems
 {
@@ -6,28 +6,8 @@ namespace Algorithm_Problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
-
-            bool isPrime = true;
-            Console.WriteLine("Prime Numbers Between 0 To 1000 :- ");
-            for (int i = 2; i <= 1000; i++)
-            {
-                for (int j = 2; j <= 1000; j++)
-                {
-
-                    if (i != j && i % j == 0)
-                    {
-                        isPrime = false;
-                        break;
-                    }
-
-                }
-                if (isPrime)
-                {
-                    Console.Write(i + " ");
-                }
-                isPrime = true;
-            }
+            PrimeNumbers numbers = new PrimeNumbers();
+            numbers.Prime();
         }
     }
 }

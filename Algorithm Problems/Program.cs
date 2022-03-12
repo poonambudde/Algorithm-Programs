f37518d1288414108e4dc340f3d3e8a6fac7c461
poonambudde <buddepoonam123@gmail.com>
2022-03-12 07:37:39 +0530

    [Poonam] ADDED: static functions to return [Poonam] ADDED: find your number

--- Algorithm Problems/Program.cs
+++ Algorithm Problems/Program.cs
@@ -7,31 +7,12 @@ namespace Algorithm_Problems
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
-
-            string str1 = "heart";
-            string str2 = "earth";
-            char[] ch1 = str1.ToLower().ToCharArray();
-            char[] ch2 = str2.ToLower().ToCharArray();
-            Array.Sort(ch1);
-            Array.Sort(ch2);
-            string val1 = new string(ch1);
-            string val2 = new string(ch2);
-
-            if (val1 == val2)
-            {
-                Console.WriteLine("Both the strings are Anagrams");
-            }
-            else
-            {
-                Console.WriteLine("Both the strings are not Anagrams");
-            }
             Console.WriteLine("Welcome to Data Sructures Algorithm Programs");
 
-            Console.WriteLine("Enter String to find Permutation ");
-            string str = Console.ReadLine();
-            int n = str.Length;
-            Permutations.Permutate(str, 0, n - 1);
+            FindNumber findNumber = new FindNumber();
+            findNumber.Find();
+
+            //findNumber.Question(12, 23);
         }
     }
 }

834ff3a370d6f3977822c84d21a979d5bb44c52e
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-12 15:04:43 +0530

    [Poonam] ADDED: Binary Search the Word from Word List

--- Algorithm Problems/Program.cs
+++ Algorithm Problems/Program.cs
@@ -5,41 +5,10 @@ namespace Algorithm_Problems
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
-
-            List<string> list = new List<string>();
-            int n;
-            Console.WriteLine("Enter the size of list");
-            n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter words in the list");
-            for (int k = 0; k < n; k++)
-            {
-                list.Add(Console.ReadLine());
-            }
-            Console.WriteLine("Plz Enter the word that has to be searched");
-            string word = Console.ReadLine();
-            list.Sort();
-            int i = 0, j = n - 1;
-            bool found = false;
-            while (i <= j)
-            {
-                int mid = (i + j) / 2;
-                if (list[mid].CompareTo(word) == 0)
-                {
-                    found = true;
-                    break;
-                }
-                else if (list[mid].CompareTo(word) < 0)
-                    i = mid + 1;
-                else
-                    j = mid - 1;
-            }
-            if (found == true)
-                Console.WriteLine("Word is Found");
-            else
-                Console.WriteLine("Word is not found");
+            BinarySearch binary = new BinarySearch();
+            binary.Search();
         }
     }
 }
